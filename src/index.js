d2729ffdc627889afb365143240b6d57f87749b5
Tore Bjolseth <tbjolset@cisco.com>
2018-09-15 18:57:29 +0200

    score tweaks

--- src/index.js
+++ src/index.js
@@ -17,13 +17,11 @@ function update() {
   updateScores(state);
   if (isPlaying) {
     nextStep(state);
-    $('.score1').style.display = 'none';
-    $('.score2').style.display = 'none';
+    $('.scores').style.display = 'none';
     window.requestAnimationFrame(update);
   }
   else {
-    $('.score1').style.display = 'flex';
-    $('.score2').style.display = 'flex';
+    $('.scores').style.display = 'block';
   }
   if (gameOver) {
     const { scores } = state;
