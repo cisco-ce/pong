80e2b588b51b055e7a60b80e8460c7dd4b3f5bd0
Tore Bjolseth <tbjolset@cisco.com>
2018-09-15 18:43:17 +0200

    tweak speeds for ipad

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
   colors: ['black', '#ffb300', '#ff1a1b', '#e900d8', '#00c4e8', '#00e258'],
-  speedIncreasePerHit: 0.5,
   ballSize: {
    w: 90,
    h: 90,
  },
- startSpeed: 4,
+ startSpeed: 7,
  maxSpeed: 20,
+ speedIncreasePerHit: 0.8,
  initYSpeed: { min: 1.5, max: 3 },
  maxBarHeight: 150,
  winScore: 5,
